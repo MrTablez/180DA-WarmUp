34c9a425d6d4fb03bf76edbbf48e52f6c14e3e18
MrTablez <loganvannevl17@gmail.com>
2023-10-11 23:41:12 -0700

    Update colortracker.py

--- colortracker.py
+++ colortracker.py
@@ -14,16 +14,23 @@ while(1):
  mask = cv.inRange(hsv, lower_blue, upper_blue)
  edges = cv.Canny(mask, 0, 999)
  #cnt = cv.findContours(edges, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
- contours,hierarchy = cv.findContours(edges, 1, 2)
- cnt = contours[0]
- x,y,w,h = cv.boundingRect(cnt)
- cv.rectangle(edges,(x,y),(x+w,y+h),(0,255,0),2)
+ contours, _ = cv.findContours(edges, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+ contours_poly = [None]*len(contours)
+ boundRect = [None]*len(contours)
+ for i, c in enumerate(contours):
+  contours_poly[i] = cv.approxPolyDP(c, 3, True)
+  boundRect[i] = cv.boundingRect(contours_poly[i])
+ drawing = np.zeros((edges.shape[0], edges.shape[1], 3), dtype=np.uint8)
+ for i in range(len(contours)):
+  color = (rng.randint(0,256), rng.randint(0,256), rng.randint(0,256))
+  cv.drawContours(drawing, contours_poly, i, color)
+  cv.rectangle(drawing, (int(boundRect[i][0]), int(boundRect[i][1])), (int(boundRect[i][0]+boundRect[i][2]), int(boundRect[i][1]+boundRect[i][3])), color, 2)
+  cv.imshow('Contours', drawing)
  # Bitwise-AND mask and original image
  #res = cv.bitwise_and(frame,frame, mask= mask)
  cv.imshow('frame',frame)
  cv.imshow('mask',mask)
  cv.imshow('edges',edges)
- cv.imshow('contours',contours)
  #cv.imshow('res',res)
  k = cv.waitKey(5) & 0xFF
  if k == 27:
