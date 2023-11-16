02576f9fdff0bca179f380353049c3a89b0c8d79
MrTablez <loganvannevl17@gmail.com>
2023-11-15 20:33:07 -0800

    Update GreenStrikerExtraction.py

--- GreenStrikerExtraction.py
+++ GreenStrikerExtraction.py
@@ -1,24 +1,42 @@
 import cv2 as cv
 import numpy as np
 
+flag = 0
 cap = cv.VideoCapture(0)
 while(1):
- # Take each frame
- _, frame = cap.read()
- # Convert BGR to HSV
- hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
- lower_green = np.array([40,51,51])
- upper_green = np.array([85,230,153])
- # Threshold the HSV image to get only green colors
- mask = cv.inRange(hsv, lower_green, upper_green)
- blur = cv.medianBlur(mask,5)
- blur2 = cv.blur(blur,(5,5))
- cv.imshow('frame',frame)
- cv.imshow('mask',mask)
- cv.imshow('blur',blur)
- cv.imshow('blur2', blur2)
- #cv.imshow('res',res)
- k = cv.waitKey(5) & 0xFF
- if k == 27:
-    break
+    # Take each frame
+    _, frame = cap.read()
+    height, width, _ = frame.shape
+    #print(width)
+    # Convert BGR to HSV
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    lower_green = np.array([40,51,51])
+    upper_green = np.array([85,230,153])
+    # Threshold the HSV image to get only green colors
+    mask = cv.inRange(hsv, lower_green, upper_green)
+    blur = cv.medianBlur(mask,5)
+    blur2 = cv.blur(blur,(20,20))
+    edges = cv.Canny(blur2, 100, 200)
+    contours, _ = cv.findContours(blur2, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    if flag == 1 and len(contours) > 0:
+        cnt = contours[0]
+        x,y,w,h = cv.boundingRect(cnt)
+        cv.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+        centroidx = x+(w//2)
+        centroidy = y+(h//2)
+        cv.circle(frame, (centroidx, centroidy), 5, (0,0,255), -1)
+        scaled_centroidx = ((centroidx/(width//2))-1)
+        print(scaled_centroidx) # this is the value we need
+        #print(x , y, w, h)
+    cv.imshow('frame',frame)
+    cv.imshow('mask',mask)
+    cv.imshow('blur',blur)
+    cv.imshow('blur2', blur2)
+    cv.imshow('edges', edges)
+    #cv.imshow('res',res)
+    k = cv.waitKey(5) & 0xFF
+    if k == 27:
+        break
+    if flag == 0:
+        flag = 1
 cv.destroyAllWindows()
