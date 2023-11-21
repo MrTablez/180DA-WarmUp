c44a23655a2dc6608d4b9dc73f178d6998382fda
MrTablez <loganvannevl17@gmail.com>
2023-11-20 16:03:52 -0800

    Update GreenStrikerExtraction.py

--- GreenStrikerExtraction.py
+++ GreenStrikerExtraction.py
@@ -16,10 +16,21 @@ while(1):
     mask = cv.inRange(hsv, lower_green, upper_green)
     blur = cv.medianBlur(mask,5)
     blur2 = cv.blur(blur,(20,20))
-    edges = cv.Canny(blur2, 100, 200)
+    edges = cv.Canny(blur, 100, 200)
     contours, _ = cv.findContours(blur2, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     if flag == 1 and len(contours) > 0:
-        cnt = contours[0]
+        w_max=0
+        h_max = 0
+        holder = 0
+        for i in range(len(contours)):
+            cnt = contours[i]
+            x_temp,y_temp,w_temp,h_temp = cv.boundingRect(cnt)
+            if w_temp >= w_max and h_temp >= h_max:
+                w_max = w_temp
+                h_max = h_temp
+                holder = i
+        cnt = contours[holder]
+        #cnt = contours[len(contours)-1]
         x,y,w,h = cv.boundingRect(cnt)
         cv.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
         centroidx = x+(w//2)
